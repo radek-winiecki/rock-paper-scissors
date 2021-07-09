907033051404bd508af8f0d33a11581a5ec5b1b9
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-07-09 13:31:47 +0200

    Create a playMatch function in the JS file

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const game = () => {
     let pScore = 0;
     let cScore = 0;
 
+    //Start the game
+
     const startGame = () => {
         const playBtn = document.querySelector('.intro button');
         const introScreen = document.querySelector('.intro');
@@ -9,6 +11,22 @@ const game = () => {
 
         playBtn.addEventListener('click', () => {
             introScreen.classList.add('fadeOut');
-        })
+            match.classList.add("fadeIn");
+        });
+    };
+    //Play match
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+
+        //Computer options
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+        const computerNumber = Math.random() * 3;
     }
-}
+    //Is call the inner function
+    startGame();
+};
+//Start the game function
+game();
